5e60fd429c5f022c69c5ae57c84adb5744129e48
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2025-06-16 01:04:57 +0200

    remove deleteed filiales

--- resources/views/layouts/composant/filiale_detail/entreprise_travaux_industriels.blade.php
+++ resources/views/layouts/composant/filiale_detail/entreprise_travaux_industriels.blade.php
@@ -2,108 +2,108 @@
     <div class="container" data-aos="fade-up">
 
         <div class="row gy-4">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-sm-6 d-flex flex-column justify-content-center"
-                data-aos="zoom-in" data-aos-delay="100">
-                <h3><b class="h1">E</b>NPS</h3>
-                <div class="container">
-                    <div
-                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
-                        <ul>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Contact : 021.37.48.41 enpsdg@yahoo.fr
-                                 </span>
-                            </li>
-                            <li><i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Affiliation : Groupe GITRAMA
-                                 </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Dénomination : ENTREPRISE NATIONALE DES PANNEAUX DE SIGNALISATION
-                                </span>
-                            </li>
-
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>   - Sigle commercial : ENPS </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>   - Date de création : 19/05/1987   </span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>   - Capital social : 198 852 000 </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>    - Nom du DG : Mr. SARDI Kamel
-                                </span>
-                            </li>
-                            {{-- <li> <i class="bi bi-browser-chrome"></i>
-                                <span><a href="http://meditram.dz/" target="_blank">
-                                    Pour  plus d'informations, visitez le site  web
-                                    </a>
-                                </span>
-                            </li> --}}
-                      </ul>
-                    </div>
-                </div>
-
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"
-                data-aos-delay="200">
-                <img src="/img/icons_filiales/enps.png" alt=""
-                    class="values_img">
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center"
-                data-aos="zoom-in" data-aos-delay="100">
-                <h3><b class="h1">G</b>ITRAMA MOUNCHAAT FOULADIA</h3>
-
-                <div class="container">
-                    <div
-                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
-                        <ul>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Contact : 034 19 27 29 sec-gmf@gitrama.dz
-                                 </span>
-                            </li>
-                            <li><i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Affiliation : Groupe GITRAMA
-                                 </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                    - Dénomination : GITRAMA MOUNCHAAT FOULADHIA
-                                </span>
-                            </li>
-
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>     - Sigle commercial : GMF </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>     - Date de création : 35/03/2022 </span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>  - Capital social : 1 000 000 </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>  - Nom du DG : M. NABET Faouzi
-                                </span>
-                            </li>
-                            <li> <i class="bi bi-browser-chrome"></i>
-                                <span><a href="http://www.aldiph.dz/index.html" target="_blank">
-                                    Pour  plus d'informations, visitez le site  web
-                                    </a>
-                                </span>
-                            </li>
-                      </ul>
-                    </div>
-                </div>
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"
-                data-aos-delay="200">
-                <img src="/img/icons_filiales/gmf.jfif" alt=""
-                    class="values_img">
-            </div><!-- End feature item-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6 col-sm-6 d-flex flex-column justify-content-center"-->
+<!--                data-aos="zoom-in" data-aos-delay="100">-->
+<!--                <h3><b class="h1">E</b>NPS</h3>-->
+<!--                <div class="container">-->
+<!--                    <div-->
+<!--                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">-->
+<!--                        <ul>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Contact : 021.37.48.41 enpsdg@yahoo.fr-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li><i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Affiliation : Groupe GITRAMA-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Dénomination : ENTREPRISE NATIONALE DES PANNEAUX DE SIGNALISATION-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!---->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>   - Sigle commercial : ENPS </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>   - Date de création : 19/05/1987   </span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>   - Capital social : 198 852 000 </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>    - Nom du DG : Mr. SARDI Kamel-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!--                            {{-- <li> <i class="bi bi-browser-chrome"></i>-->
+<!--                                <span><a href="http://meditram.dz/" target="_blank">-->
+<!--                                    Pour  plus d'informations, visitez le site  web-->
+<!--                                    </a>-->
+<!--                                </span>-->
+<!--                            </li> --}}-->
+<!--                      </ul>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"-->
+<!--                data-aos-delay="200">-->
+<!--                <img src="/img/icons_filiales/enps.png" alt=""-->
+<!--                    class="values_img">-->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center"-->
+<!--                data-aos="zoom-in" data-aos-delay="100">-->
+<!--                <h3><b class="h1">G</b>ITRAMA MOUNCHAAT FOULADIA</h3>-->
+<!---->
+<!--                <div class="container">-->
+<!--                    <div-->
+<!--                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">-->
+<!--                        <ul>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Contact : 034 19 27 29 sec-gmf@gitrama.dz-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li><i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Affiliation : Groupe GITRAMA-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                    - Dénomination : GITRAMA MOUNCHAAT FOULADHIA-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!---->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>     - Sigle commercial : GMF </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>     - Date de création : 35/03/2022 </span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>  - Capital social : 1 000 000 </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>  - Nom du DG : M. NABET Faouzi-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-browser-chrome"></i>-->
+<!--                                <span><a href="http://www.aldiph.dz/index.html" target="_blank">-->
+<!--                                    Pour  plus d'informations, visitez le site  web-->
+<!--                                    </a>-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!--                      </ul>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"-->
+<!--                data-aos-delay="200">-->
+<!--                <img src="/img/icons_filiales/gmf.jfif" alt=""-->
+<!--                    class="values_img">-->
+<!--            </div><!-- End feature item-->-->
 
         </div>
     </div>
--- resources/views/layouts/composant/filiale_detail/entreprise_travaux_maritimes.blade.php
+++ resources/views/layouts/composant/filiale_detail/entreprise_travaux_maritimes.blade.php
@@ -104,94 +104,94 @@
                 <img src="/img/icons_filiales/aldiph.png" alt=""
                     class="values_img">
             </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center "
-                data-aos="zoom-in" data-aos-delay="100">
-                <h3><b class="h1">S</b>OTRAMO</h3>
-
-                <div class="container">
-                    <div
-                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
-                        <ul>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                - Contact : 041.82.25.68  sotramoalg@yahoo.f
-                                 </span>
-                            </li>
-                            <li><i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                - Affiliation : Groupe GITRAMA Spa
-                                 </span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>
-                                - Dénomination : SOCIETE DE TRAVAUX MARTIMES DE L'OUEST
-                                </span>
-                            </li>
-
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>  - Sigle commercial : SOTRAMO</span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>  - Date de création : 23/02/1980 </span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                 <span>  - Capital social : 280 000 000</span>
-                            </li>
-                            <li> <i class="bi bi-caret-right-fill"></i>
-                                <span>  - Nom du PDG : Mr. KALOULI Mustapha Kamel
-                                </span>
-                            </li>
-                      </ul>
-                    </div>
-                </div>
-
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"
-                data-aos-delay="200">
-                <img src="/img/icons_filiales/sotramo.png" alt=""
-                    class="values_img">
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center"
-                data-aos="zoom-in" data-aos-delay="100">
-                <h3><b class="h1">S</b>otramest</h3>
-                <div class="container">
-                    <div
-                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
-                        <ul>
-                            <li> <i class="bi bi-caret-right-fill"></i> <span>
-                                    038.48.57.58 - sotramestdg@gmail.com</span></li>
-                            <li><i class="bi bi-caret-right-fill"></i> <span>-
-                                    Affiliation : Groupe GITRAMA Spa</span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i> <span> -
-                                    Dénomination : SOCIETE DE TRAVAUX MARTIMES DE
-                                    L'EST</span></li>
-
-                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Sigle
-                                    commercial : SOTRAMEST</span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Date de
-                                    création : 23/02/1980</span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Capital
-                                    social : 2 200 000 000</span></li>
-                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Nom du
-                                    DG : M. MAATOUGUI Noureddine</span></li>
-                            <li> <i class="bi bi-browser-chrome"></i>
-                                <span>
-                                <a href="https://site-sotramest-19.webself.net/"  target="_blank" >
-                                    Pour plus d'informations, visitez le site  web
-                                </a>
-
-                                </span>
-                            </li>
-
-
-                        </ul>
-                    </div>
-                </div>
-            </div><!-- End feature item-->
-            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"
-                data-aos-delay="200">
-                <img src="/img/icons_filiales/sotramest.png" alt=""
-                    class="values_img">
-            </div><!-- End feature item-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center "-->
+<!--                data-aos="zoom-in" data-aos-delay="100">-->
+<!--                <h3><b class="h1">S</b>OTRAMO</h3>-->
+<!---->
+<!--                <div class="container">-->
+<!--                    <div-->
+<!--                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">-->
+<!--                        <ul>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                - Contact : 041.82.25.68  sotramoalg@yahoo.f-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li><i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                - Affiliation : Groupe GITRAMA Spa-->
+<!--                                 </span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>-->
+<!--                                - Dénomination : SOCIETE DE TRAVAUX MARTIMES DE L'OUEST-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!---->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>  - Sigle commercial : SOTRAMO</span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>  - Date de création : 23/02/1980 </span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                 <span>  - Capital social : 280 000 000</span>-->
+<!--                            </li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i>-->
+<!--                                <span>  - Nom du PDG : Mr. KALOULI Mustapha Kamel-->
+<!--                                </span>-->
+<!--                            </li>-->
+<!--                      </ul>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"-->
+<!--                data-aos-delay="200">-->
+<!--                <img src="/img/icons_filiales/sotramo.png" alt=""-->
+<!--                    class="values_img">-->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center"-->
+<!--                data-aos="zoom-in" data-aos-delay="100">-->
+<!--                <h3><b class="h1">S</b>otramest</h3>-->
+<!--                <div class="container">-->
+<!--                    <div-->
+<!--                        class=" order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">-->
+<!--                        <ul>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span>-->
+<!--                                    038.48.57.58 - sotramestdg@gmail.com</span></li>-->
+<!--                            <li><i class="bi bi-caret-right-fill"></i> <span>--->
+<!--                                    Affiliation : Groupe GITRAMA Spa</span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span> --->
+<!--                                    Dénomination : SOCIETE DE TRAVAUX MARTIMES DE-->
+<!--                                    L'EST</span></li>-->
+<!---->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Sigle-->
+<!--                                    commercial : SOTRAMEST</span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Date de-->
+<!--                                    création : 23/02/1980</span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Capital-->
+<!--                                    social : 2 200 000 000</span></li>-->
+<!--                            <li> <i class="bi bi-caret-right-fill"></i> <span> - Nom du-->
+<!--                                    DG : M. MAATOUGUI Noureddine</span></li>-->
+<!--                            <li> <i class="bi bi-browser-chrome"></i>-->
+<!--                                <span>-->
+<!--                                <a href="https://site-sotramest-19.webself.net/"  target="_blank" >-->
+<!--                                    Pour plus d'informations, visitez le site  web-->
+<!--                                </a>-->
+<!---->
+<!--                                </span>-->
+<!--                            </li>-->
+<!---->
+<!---->
+<!--                        </ul>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div><!-- End feature item-->-->
+<!--            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in"-->
+<!--                data-aos-delay="200">-->
+<!--                <img src="/img/icons_filiales/sotramest.png" alt=""-->
+<!--                    class="values_img">-->
+<!--            </div><!-- End feature item-->-->
         </div>
     </div>
 </section>
